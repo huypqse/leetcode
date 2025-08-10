2215dcab67c1a65654a5fcf0b13d57371405c1c2
Phan Quang Huy <huypqse@gmail.com>
2025-08-11 00:53:08 +0700

    feat: do strings for daily

---
+++ strings/detect_capitals.go
@@ -0,0 +1,15 @@
+import (
+	"unicode"
+)
+
+func detectCapitalUse(word string) bool {
+	n := len(word)
+	count := 0
+	for _, ch := range word {
+		if unicode.IsUpper(ch) {
+			count++
+		}
+	}
+	return count == n || count == 0 || (count == 1 && unicode.IsUpper(rune(word[0])))
+
+}
